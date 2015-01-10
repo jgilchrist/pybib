c584e9a802f5f5ca2a5ab6c15e1819385631b381
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-01-10 21:13:34 +0000

    Improve feedback to user

--- pybib/pybib.py
+++ pybib/pybib.py
@@ -16,17 +16,19 @@ bibtex_key_matcher = re.compile('@\w+{(\w+),')
 def convert_file(filename, driver):
     """Convert a file in the format [citation_key doi_key]* into a valid bibtex file"""
 
-    citation_key_to_doi = parse_file(filename)
+    print("Retrieving entries from {}".format(driver.url))
 
-    citation_key_to_bib = get_all_bibliography_data(citation_key_to_doi, driver)
+    doi_entries = parse_file(filename)
 
-    bibtex_lines = replace_citation_keys(citation_key_to_bib)
+    bib_entries = get_all_bibliography_data(doi_entries, driver)
+
+    bib_entries = replace_citation_keys(bib_entries)
 
     (root, ext) = os.path.splitext(filename)
     bibtex_file = root + ".bib"
 
     with open(bibtex_file, 'w') as bib:
-        for citation in bibtex_lines:
+        for citation in bib_entries:
             print(citation, file=bib)
 
     print("Wrote generated BibTeX file as {}".format(bibtex_file))
@@ -34,15 +36,14 @@ def convert_file(filename, driver):
 def parse_file(filename):
     """Parses a file in the format [citation_key doi_key]* into a dictionary {citation_key: doi_key}*"""
 
-    citation_key_to_doi = {}
+    doi_entries = {}
 
-    f = open(filename)
-    lines = f.readlines()
-    f.close()
+    with open(filename) as f:
+        lines = f.readlines()
 
-    for line_number, line in enumerate(lines, start=1):
+    for i, line in enumerate(lines, start=1):
 
-        parsed_line = parse_line(line_number, line)
+        parsed_line = parse_line(i, line)
 
         if parsed_line is None:
             # Line was blank, or a comment
@@ -51,14 +52,14 @@ def parse_file(filename):
         citation_key, doi = parsed_line
 
         # Don't allow duplicate citation keys
-        if citation_key in citation_key_to_doi:
-            sys.exit("Duplicate citation key: {} on line number {}".format(citation_key, line_number))
+        if citation_key in doi_entries:
+            error("Line {}: duplicate citation key: {}".format(i, citation_key))
 
-        citation_key_to_doi[citation_key] = doi
+        doi_entries[citation_key] = doi
 
-    print("Found {} citations in {}".format(len(citation_key_to_doi), filename))
+    print("Found {} citations in {}".format(len(doi_entries), filename))
 
-    return citation_key_to_doi
+    return doi_entries
 
 def parse_line(line_number, line):
     """Parses a single line, of the format [citation_key DOI]"""
@@ -73,32 +74,32 @@ def parse_line(line_number, line):
     parsed_line = key_file_matcher.match(line)
 
     if not parsed_line:
-        sys.exit("Invalid line ({}) in file: {}".format(line_number, line.strip()))
+        error("Line {}: Syntax error ({})".format(line_number, line.strip()))
 
     citation_key, doi = parsed_line.group(1, 2)
 
     # Limit the length of citation keys
     if len(citation_key) > 20:
-        sys.exit("Citation key '{}' is over 20 characters".format(citation_key))
+        error("Line {}: Citation key '{}' is over 20 characters".format(line_number, citation_key))
 
     if not doi_matcher.match(doi):
-        sys.exit("Invalid doi key.")
+        error("Line {}: Invalid DOI '{}'".format(line_number, doi))
 
     return citation_key, doi
 
-def get_all_bibliography_data(citation_key_to_doi, driver):
+def get_all_bibliography_data(doi_entries, driver):
     """Takes a dict in the format {citation_key: doi_key}* and expands each doi key into its bibliography text"""
 
-    print("Retrieving bibliography data... ", end="")
+    print("Retrieving bibliography entries:")
 
     key_to_bibliography = {}
 
-    for citation_key, doi in citation_key_to_doi.items():
+    for citation_key, doi in doi_entries.items():
+        print("\t{} ({}) -> ".format(citation_key, doi), end="")
         bibliography_text = driver.get_entry(doi)
+        print("Done.")
 
         key_to_bibliography[citation_key] = bibliography_text
-
-    print("Done")
 
     return key_to_bibliography
 
@@ -115,7 +116,7 @@ def replace_citation_keys(key_to_bibliography):
         citation_key_match = bibtex_key_matcher.match(bib_text)
 
         if not citation_key_match:
-            sys.exit("Ill formatted bibtex entry:\n{}".format(bib_text))
+            error("Ill formatted bibtex entry:\n{}".format(bib_text))
 
         old_key = citation_key_match.group(1)
 
@@ -124,3 +125,6 @@ def replace_citation_keys(key_to_bibliography):
         bib_items.append(new_bib)
 
     return bib_items
+
+def error(msg):
+    sys.exit(msg)
