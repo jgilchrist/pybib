fd66ab0a160cc145312b52cff69ca7d14b6da717
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-01-11 21:16:18 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pybib',
-      version='1.0',
+      version='1.1',
       description='Auto-generate a BibTeX file from a list of Digital Object Identifiers',
       url='https://github.com/jgilchrist/pybib',
       author='Jonny Gilchrist',
