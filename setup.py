ad19746ba86fb44c838b573a34a73198a8f30eaf
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-05-17 14:44:32 +0100

    Bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pybib',
-      version='1.2.2',
+      version='1.3.0',
       description='Fetch citation information, given a Digital Object Identifier',
       url='https://github.com/jgilchrist/pybib',
       author='Jonny Gilchrist',
