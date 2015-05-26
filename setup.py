6a84b18f584c7f9b8a3d7d53605bce5be919b056
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-05-26 21:22:56 +0100

    Fix the README for PyPi

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from pybib import __version__
 setup(name='pybib',
       version=__version__,
       description='Fetch citation information, given a Digital Object Identifier',
+      long_description=open('README.rst').read(),
       url='https://github.com/jgilchrist/pybib',
       author='Jonny Gilchrist',
       packages=['pybib'],
