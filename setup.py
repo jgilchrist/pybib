f5b0203b5651f25246b79896da2c002b8ddad4d6
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-05-29 17:36:33 +0100

    Use a context handler for reading in README

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 from pybib import __version__
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(name='pybib',
       version=__version__,
       description='Fetch citation information, given a Digital Object Identifier',
-      long_description=open('README.rst').read(),
+      long_description=readme,
       url='https://github.com/jgilchrist/pybib',
       author='Jonny Gilchrist',
       packages=['pybib'],
