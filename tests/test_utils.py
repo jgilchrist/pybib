6d8e9468ab16f53febb66c5559e601fc759af9f5
Jonny Gilchrist <jonnygilchrist@gmail.com>
2015-09-15 14:03:46 +0100

    Add tests for the search and get_bibtex utilities
    These interact with requests, so they required requests_mock to test.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,9 @@ from hamcrest import *
 
 from nose.tools import assert_raises
 
+import requests
+import requests_mock
+
 class MockRequest:
     def __init__(self, code):
         self.code = code
@@ -21,3 +24,54 @@ def test_handle_status_code_404():
 def test_handle_status_code_unknown():
     with assert_raises(SystemExit):
         utils.handle_status_code(MockRequest(1))
+
+def test_search():
+    search_json_response = """
+    {
+        "message-version": "1.0.0",
+        "message": {
+            "facets": {},
+            "query": {
+                "start-index": 0,
+                "search-terms": "test"
+            },
+            "total-results": 1,
+            "items": [{
+                "source": "CrossRef",
+                "title": ["Test Citation"],
+                "type": "dissertation", "URL": "http://dx.doi.org/test.doi",
+                "deposited": {"timestamp": 1000, "date-parts": [[2015, 1, 1]]},
+                "container-title": [],
+                "author": [{"given": "Test", "affiliation": [], "family": "Test"}],
+                "reference-count": 0,
+                "member": "http://id.crossref.org/member/xxx",
+                "subtitle": [],
+                "indexed": { "timestamp": 1000, "date-parts": [[2015, 1, 1]] },
+                "prefix": "http://id.crossref.org/prefix/test",
+                "publisher": "Test Publisher",
+                "score": 1.0,
+                "DOI": "test.doi",
+                "issued": { "date-parts": [[]] }
+            }]
+        }
+    }
+    """
+
+    with requests_mock.mock() as m:
+        m.get('http://api.crossref.org/works', text=search_json_response)
+        entries = utils.search('test.doi')
+
+
+        print(entries)
+
+    assert_that(len(entries), equal_to(1))
+
+    entry = entries[0]
+    assert_that(entry["title"], equal_to(["Test Citation"]))
+
+def test_get_bibtex():
+    with requests_mock.mock() as m:
+        m.get('http://dx.doi.org/test.doi', text='abc')
+        entry = utils.get_bibtex('test.doi')
+
+    assert_that(entry, equal_to('abc'))
