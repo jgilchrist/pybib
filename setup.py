8454dab19d4e80b3d103cc55c99f045ad1591116
Jonathan Gilchrist <jonnygilchrist@gmail.com>
2018-03-10 11:51:05 +0000

    Update package metadata
    - Display license on pypi
    - Use find_packages()

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 from pybib import __version__
 
@@ -6,19 +6,20 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(name='pybib',
-      version=__version__,
-      description='Fetch citation information, given a Digital Object Identifier',
-      long_description=readme,
-      url='https://github.com/jgilchrist/pybib',
-      author='Jonathan Gilchrist',
-      packages=['pybib'],
-      install_requires=[
-          'requests',
-          'colorama'
-      ],
-      entry_points = {
-          'console_scripts': [
-              'bib = pybib:main'
-          ]
-      }
+    version=__version__,
+    description='Fetch citation information, given a Digital Object Identifier',
+    long_description=readme,
+    url='https://github.com/jgilchrist/pybib',
+    author='Jonathan Gilchrist',
+    license='BSD 3-Clause License',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'colorama'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'bib = pybib:main'
+        ]
+    }
 )
